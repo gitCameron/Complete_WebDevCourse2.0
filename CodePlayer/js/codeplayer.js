18d8f1d6c83a5b6d4f50bf04022eb73678a76439
Cameron Cummings <cscummings5@yahoo.com>
2017-12-19 12:10:27 -0700

    Finished panels

--- CodePlayer/js/codeplayer.js
+++ CodePlayer/js/codeplayer.js
@@ -1,4 +1,9 @@
 
+function updateOutput() {
+
+	$("iframe").contents().find('html').html("<html><head><style type='text/css'>" + $("#cssPanel").val()
+		+ "</style></head><body>" + $("#htmlPanel").val() + "</body></html>"); 
+}
 
 $(".toggleButton").hover(function(){
 
@@ -13,12 +18,30 @@ $(".toggleButton").hover(function(){
 $(".toggleButton").click(function() {
 
 	$(this).toggleClass("active");
+
 	$(this).removeClass("highlightedButton");
+
+	var panelId = $(this).attr("id") + "Panel";
+	$("#" + panelId).toggleClass("hidden");
+
+	var numberOfActivePanels = 4 - $('hidden').length;
+	$(".panel").width(($(window).width() / numberOfActivePanels) - 10);
 
 });
 
 $(".panel").height($(window).height() - $("#topBar").height() - 15);
 
-$(".panel").width(($(window).width() / 2) - 10);
+updateOutput();
 
-$("iframe").contents().find('html').html($("#htmlPanel").val()); 
+$("textarea").on('change keyup paste', function() {
+
+	updateOutput();
+
+});
+
+$(window).resize(function() {
+
+	var numberOfActivePanels = 4 - $('hidden').length;
+	$(".panel").width(($(window).width() / numberOfActivePanels) - 10);
+});
+
