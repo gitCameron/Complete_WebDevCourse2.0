9cab36e2a2919ab8c2ad0d419040c10bb4f305ac
Cameron Cummings <cscummings5@yahoo.com>
2018-01-05 17:04:34 -0700

    Finished CodePlayer app

--- CodePlayer/js/codeplayer.js
+++ CodePlayer/js/codeplayer.js
@@ -3,6 +3,8 @@ function updateOutput() {
 
 	$("iframe").contents().find('html').html("<html><head><style type='text/css'>" + $("#cssPanel").val()
 		+ "</style></head><body>" + $("#htmlPanel").val() + "</body></html>"); 
+
+	document.getElementById("outputPanel").contentWindow.eval($("#javascriptPanel").val())
 }
 
 $(".toggleButton").hover(function(){
@@ -16,27 +18,30 @@ $(".toggleButton").hover(function(){
 });
 
 $(".toggleButton").click(function() {
+                
+    $(this).toggleClass("active");
+    
+    $(this).removeClass("highlightedButton");
+    
+    var panelId = $(this).attr("id") + "Panel";
+    
+    $("#" + panelId).toggleClass("hidden");
+    
+    var numberOfActivePanels = 4 - $('.hidden').length;
+    
+    $(".panel").width(($(window).width() / numberOfActivePanels) - 10);
+    
+})
 
-	$(this).toggleClass("active");
+$(".panel").height($(window).height() - $("#header").height() - 15);
 
-	$(this).removeClass("highlightedButton");
-
-	var panelId = $(this).attr("id") + "Panel";
-	$("#" + panelId).toggleClass("hidden");
-
-	var numberOfActivePanels = 4 - $('hidden').length;
-	$(".panel").width(($(window).width() / numberOfActivePanels) - 10);
-
-});
-
-$(".panel").height($(window).height() - $("#topBar").height() - 15);
+$(".panel").width(($(window).width() / 2) - 10);
 
 updateOutput();
 
 $("textarea").on('change keyup paste', function() {
 
-	updateOutput();
-
+    updateOutput();  
 });
 
 $(window).resize(function() {
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 This repository is for the sole purpose of storing project(s) from an online web developer course.  
 The course covers topics such as HTML, CSS, Javascript, JQuery, Bootstrap 4, Wordpress, PHP, MySQL, APIs, and Python.
+
+## CodePlayer (JQuery)
