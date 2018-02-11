442f1c5e159222a13f1d3e79259be9f306733559
Cameron Cummings <cscummings5@yahoo.com>
2018-02-10 20:24:07 -0700

    Continued building app with bootstrap and changed directory name

--- MyRentalPropertyApp/scripts/home.js
+++ MyRentalPropertyApp/scripts/home.js
@@ -3,7 +3,7 @@ var jumboHeight = $('.jumbotron').outerHeight();
 
 function parallax(){
 	var scrolled = $(window).scrollTop();
-	$('.jumbotron').css('height', (jumboHeight-(scrolled/4)) + 'px');
+	$('.jumbotron').css('height', (jumboHeight-(scrolled/6)) + 'px');
 }
 
 $(window).scroll(function(){
@@ -13,4 +13,10 @@ $(window).scroll(function(){
 $('nav li').click(function(e){
 	$('.active').removeClass('active');
 	$(this).addClass('active');
+});
+
+$(document).ready(function(){
+	$(".loginButton").click(function(){
+		$("#myModal").modal();
+	});
 });
